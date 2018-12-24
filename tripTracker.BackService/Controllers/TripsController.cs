0d27de8eecf93dcd08828ca2b0dd6a7d81698931
DESKTOP-7BOO3DJ\Lenovo <AHarake@spartaglobal.com>
2018-12-24 12:44:54 +0000

    connecti entity framework and fixed some errors

--- tripTracker.BackService/Controllers/TripsController.cs
+++ tripTracker.BackService/Controllers/TripsController.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using tripTracker.BackService.Data;
 using tripTracker.BackService.Models;
+using TripTracker.BackService.Data;
 
 namespace tripTracker.BackService.Controllers
 {
