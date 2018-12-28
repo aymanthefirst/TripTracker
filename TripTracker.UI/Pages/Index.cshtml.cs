165f76f626f63d57f262b4cc5a92848e5d4c49eb
DESKTOP-7BOO3DJ\Lenovo <AHarake@spartaglobal.com>
2018-12-28 13:44:14 +0000

    AutoSave - latest working version

--- TripTracker.UI/Pages/Index.cshtml.cs
+++ TripTracker.UI/Pages/Index.cshtml.cs
@@ -4,25 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using TripTracker.BackService.Models;
 
 namespace TripTracker.UI.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly TripTracker.UI.Data.ApplicationDbContext _context;
-
-        public IndexModel(TripTracker.UI.Data.ApplicationDbContext context)
+        public void OnGet()
         {
-            _context = context;
-        }
-
-        public IList<Trip> Trip { get; set; }
-
-        public async Task GetTaskAsync()
-        {
-            Trip = await _context.Trip.ToListAsync();
 
         }
     }
